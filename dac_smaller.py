00762f30e996c843e67e1eafbc1923da9945b69d
jeostro <156924263+jeostro@users.noreply.github.com>
2024-05-06 14:45:59 -0400

    update

---
+++ dac_smaller.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 26 18:27:22 2024
+
+Script 3/6
+Purpose: Filter DAC data to only a few columns, so the size is more manageable 
+    for later merges. Create geopackage.
+    
+Required input files:
+    -"dac_designation.gpkg"
+    
+@author: jeostro
+
+"""
+    
+# Import modules
+
+import pandas as pd
+import geopandas as gpd
+
+# Read joined DAC data
+
+dac_sm = gpd.read_file("dac_designation.gpkg")
+
+# Save only a few columns to make file smaller
+
+dac_sm = dac_sm[["tract","geometry","pop_total","Household_Count"]]
+
+# Check dataframe
+
+print(dac_sm)
+
+# Save "dac_sm" as geopackage
+
+dac_sm.to_file("dac_designation_sm.gpkg",layer="dac_sm")
